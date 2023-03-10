c6a25d437cc100f795eb48c0f9dd6e51f15bd765
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-03-10 15:57:30 +0200

    FOOT-9: Create home screen shell

---
+++ app/src/main/java/com/example/football/ui/feature/HomeScreenFragment.kt
@@ -0,0 +1,43 @@
+package com.example.football.ui.feature
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.example.football.ui.theme.FootballTheme
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
+
+@AndroidEntryPoint
+class HomeScreenFragment : Fragment() {
+
+    @Inject
+    lateinit var viewModel: HomeScreenViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                FootballTheme {
+                    HomeScreen()
+                }
+            }
+        }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.initialise()
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        //viewModel = ViewModelProvider(this).get(HomescreenViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+}
