9a64478cd297a904c69ea18af433339110439545
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-03-10 15:59:38 +0200

    FOOT-16: Init nav host in fragment container on main activity

---
+++ app/src/main/java/com/example/football/ui/feature/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.football.ui.feature
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import com.example.football.R
+import com.example.football.databinding.ActivityMainScreenBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainScreenBinding
+    private lateinit var navHostFragment: NavHostFragment
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+    }
+}
