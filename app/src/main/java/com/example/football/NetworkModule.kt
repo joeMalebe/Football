dd23cb308e167f78d8aacb31c9395836e7bd7c6a
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-03-26 21:44:57 +0200

    FOOT-15: add result call adapter and logging interceptor

--- app/src/main/java/com/example/football/NetworkModule.kt
+++ app/src/main/java/com/example/football/NetworkModule.kt
@@ -1,41 +1,57 @@
 package com.example.football
 
+import com.example.football.exception.ResultCallAdaptorFactory
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.Dispatchers
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import kotlin.coroutines.CoroutineContext
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+
+    private val json = Json { ignoreUnknownKeys = true }
 
     @OptIn(ExperimentalSerializationApi::class)
     @Provides
     fun footballService(okHttpClient: OkHttpClient): FootballService {
         val contentType = "application/json".toMediaType()
         return Retrofit.Builder()
-            .addConverterFactory(Json.asConverterFactory(contentType))
+            .addConverterFactory(json.asConverterFactory(contentType))
             .client(okHttpClient)
+            .addCallAdapterFactory(ResultCallAdaptorFactory())
+            .baseUrl("https://" + BuildConfig.BASE_URL)
             .build()
             .create(FootballService::class.java)
     }
 
     @Provides
     fun okHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
         return OkHttpClient().newBuilder().addInterceptor {
             it.proceed(
                 it.request().newBuilder()
-                    .url(BuildConfig.BASE_URL)
-                    .addHeader("X-RapidAPI-Key", BuildConfig.API_KEY)
-                    .addHeader("X-RapidAPI-Host", BuildConfig.BASE_URL)
+                    .addHeader("x-rapidapi-key", BuildConfig.API_KEY)
+                    .addHeader("x-rapidapi-host", BuildConfig.BASE_URL)
                     .build()
             )
-        }.build()
+        }.addInterceptor(interceptor).build()
+    }
+
+    @Provides
+    fun ioContext(): CoroutineContext {
+        return Dispatchers.IO
     }
 }
