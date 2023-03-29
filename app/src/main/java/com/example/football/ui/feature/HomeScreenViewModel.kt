5345164a15f27045b446d9b4a89eb552ce27f7d7
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-03-29 20:00:45 +0200

    FOOT-15: test service call coroutine

--- app/src/main/java/com/example/football/ui/feature/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/football/ui/feature/HomeScreenViewModel.kt
@@ -2,13 +2,21 @@ package com.example.football.ui.feature
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.football.repository.SearchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 @HiltViewModel
-class HomeScreenViewModel @Inject constructor() : ViewModel() {
+class HomeScreenViewModel @Inject constructor(private val searchRepository: SearchRepository, private val ioContext: CoroutineContext) : ViewModel() {
     fun initialise() {
+        CoroutineScope(ioContext).launch {
+            val response = searchRepository.searchCountry("engl")
+            Log.d("HomeScreenViewModel", response.toString())
+        }
         Log.d("HomeScreenViewModel","hello view model")
     }
-    // TODO: Implement the ViewModel
+    // todo: Implement the ViewModel
 }
