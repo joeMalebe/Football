3c0de9336d6da08fffc6b24290e6e7fe5ed567ce
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-03-29 19:53:12 +0200

    FOOT-15: cleanup gradle file and remove test

--- app/src/androidTest/java/com/example/football/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/football/ExampleInstrumentedTest.kt
@@ -1,12 +1,8 @@
 package com.example.football
 
-import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
 import org.junit.Assert.*
+import org.junit.runner.RunWith
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -15,10 +11,5 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.football", appContext.packageName)
-    }
+
 }
