f8f3f1ed762359e3d9207d4f8288e0f02175670e
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-04-07 21:41:49 +0200

    FOOT-3: create search bar

--- app/src/main/java/com/example/football/presentation/view/HomeScreen.kt
+++ app/src/main/java/com/example/football/presentation/view/HomeScreen.kt
@@ -1,33 +1,75 @@
 package com.example.football.presentation.view
 
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.football.R
 import com.example.football.presentation.theme.FootballTheme
 
 @Composable
 fun HomeScreen() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
+
+    var searchText by remember { mutableStateOf(TextFieldValue("")) }
+
+    Content(
+        searchText = searchText.text,
+        onSearchTextChange = { newSearchText ->
+            searchText = TextFieldValue(newSearchText)
+        })
+}
+
+@Composable
+fun Content(
+    searchText: String,
+    onSearchTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            SearchBar(
+                modifier = Modifier.fillMaxWidth(),
+                searchText = searchText,
+                onSearchTextChange = onSearchTextChange
+            )
+        }
     ) {
-        Greeting("Android")
+        it
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun SearchBar(
+    searchText: String,
+    onSearchTextChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    OutlinedTextField(
+        value = searchText,
+        onValueChange = onSearchTextChange,
+        modifier = modifier,
+        shape = CircleShape,
+        placeholder = {
+            Text(
+                text = stringResource(id = R.string.search)
+            )
+        },
+        leadingIcon = { Icon(imageVector = Icons.Outlined.Search, contentDescription = "Search") }
+    )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun DefaultPreview() {
+fun SearchBarPreview() {
     FootballTheme {
-        Greeting("Android")
+        SearchBar(searchText = "", onSearchTextChange = {})
     }
 }
