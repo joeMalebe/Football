7b4d8d9779c3efb80208fa924fd8e2cda3bf8ac9
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-04-27 13:55:29 +0200

    FOOT-30: create standings view data and mapper

--- app/src/main/java/com/example/football/domain/StandingsViewDataMapper.kt
+++ app/src/main/java/com/example/football/domain/StandingsViewDataMapper.kt
@@ -1,6 +1,38 @@
 package com.example.football.domain
 
-interface StandingsViewDataMapper
+import com.example.football.data.model.StandingsResponse
+
+interface StandingsViewDataMapper {
+    fun mapDtoToStandingsResult(standingsResponse: StandingsResponse?): StandingsResult
+}
+
+sealed class StandingsResult {
+    object Error : StandingsResult()
+
+    object NoStandingsInformation : StandingsResult()
+
+    data class StandingsLoaded(val standingsViewData: List<StandingsViewData>) : StandingsResult()
+}
 
 class StandingsViewDataMapperImpl() : StandingsViewDataMapper {
+    override fun mapDtoToStandingsResult(standingsResponse: StandingsResponse?): StandingsResult {
+        return if (standingsResponse == null) {
+            StandingsResult.NoStandingsInformation
+        } else {
+            StandingsResult.StandingsLoaded(
+                standingsResponse.response[0].standings[0].map {
+                    StandingsViewData(
+                        teamId = it.team.id,
+                        draws = it.all.draw,
+                        losses = it.all.lose,
+                        logo = it.team.logo,
+                        points = it.points,
+                        position = it.rank,
+                        positionDescription = it.description,
+                        wins = it.all.win
+                    )
+                }
+            )
+        }
+    }
 }
