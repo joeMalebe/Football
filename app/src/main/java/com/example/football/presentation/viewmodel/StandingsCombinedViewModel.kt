0e8548d12b60d3ba1636dce541948d37b2a2a79f
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-08-26 13:26:15 +0200

    FOOT-56: create combined standings view model

---
+++ app/src/main/java/com/example/football/presentation/viewmodel/StandingsCombinedViewModel.kt
@@ -0,0 +1,66 @@
+package com.example.football.presentation.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class StandingsCombinedViewModel : ViewModel() {
+
+    private val _standingsCombinedViewState =
+        MutableLiveData(StandingsCombinedViewState(combinedViewState = true))
+    val standingsCombinedViewState: LiveData<StandingsCombinedViewState>
+        get() = _standingsCombinedViewState
+
+    fun onTopGoalScorerSeeAllClicked() {
+        _standingsCombinedViewState.postValue(
+            _standingsCombinedViewState.value?.let {
+                it.setState(
+                    topGoalScorerSeeAll = !it.topGoalScorerSeeAll,
+                    combinedViewState = it.topGoalScorerSeeAll
+                )
+            }
+        )
+    }
+
+    fun onStandingsSeeAllClicked() {
+        _standingsCombinedViewState.postValue(
+            _standingsCombinedViewState.value?.let {
+                it.setState(
+                    standingsSeeAll = !it.standingsSeeAll, combinedViewState = it.standingsSeeAll
+                )
+            }
+        )
+    }
+
+    fun onLoadingComplete() {
+        _standingsCombinedViewState.postValue(
+            _standingsCombinedViewState.value?.setState(
+                combinedViewState = true,
+            )
+        )
+    }
+}
+
+data class StandingsCombinedViewState(
+    val loading: Boolean = false,
+    val combinedViewState: Boolean = false,
+    val topGoalScorerSeeAll: Boolean = false,
+    val standingsSeeAll: Boolean = false,
+    val error: Boolean = false
+) {
+    fun setState(
+        loading: Boolean = false,
+        combinedViewState: Boolean = false,
+        topGoalScorerSeeAll: Boolean = false,
+        standingsSeeAll: Boolean = false,
+        error: Boolean = false
+    ) =
+        copy(
+            loading = loading,
+            combinedViewState = combinedViewState,
+            topGoalScorerSeeAll = topGoalScorerSeeAll,
+            standingsSeeAll = standingsSeeAll,
+            error = error
+        )
+}
+
---
+++ app/src/test/java/com/example/football/presentation/viewmodel/StandingsCombinedViewModelTest.kt
@@ -0,0 +1,104 @@
+package com.example.football.presentation.viewmodel
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations.openMocks
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
+
+class StandingsCombinedViewModelTest{
+
+    val standingsCombinedViewModel = StandingsCombinedViewModel()
+
+    @Mock
+    lateinit var observer : Observer<StandingsCombinedViewState>
+
+
+    @get:Rule
+    val rule = InstantTaskExecutorRule()
+
+    @Before
+    fun setUp() {
+        openMocks(this)
+    }
+
+    @Test
+    fun `When onTopGoalScorerSeeAllClicked is called then emit opposite values for topGoalScorerSeeAll and combinedViewState`() {
+        standingsCombinedViewModel.standingsCombinedViewState.observeForever(observer)
+
+        standingsCombinedViewModel.onTopGoalScorerSeeAllClicked()
+        standingsCombinedViewModel.onTopGoalScorerSeeAllClicked()
+
+        verify(observer).onChanged(
+            StandingsCombinedViewState(
+                topGoalScorerSeeAll = true,
+                combinedViewState = false
+            )
+        )
+        verify(observer).onChanged(
+            StandingsCombinedViewState(
+                topGoalScorerSeeAll = false,
+                combinedViewState = true
+            )
+        )
+        verify(observer, times(0)).onChanged(
+            StandingsCombinedViewState(
+                topGoalScorerSeeAll = false,
+                combinedViewState = false
+            )
+        )
+        verify(observer, times(0)).onChanged(
+            StandingsCombinedViewState(
+                topGoalScorerSeeAll = true,
+                combinedViewState = true
+            )
+        )
+    }
+
+    @Test
+    fun `When onStandingsSeeAllClicked is called then emit standingsSeeAll true`() {
+        standingsCombinedViewModel.standingsCombinedViewState.observeForever(observer)
+
+        standingsCombinedViewModel.onStandingsSeeAllClicked()
+        standingsCombinedViewModel.onStandingsSeeAllClicked()
+
+        verify(observer).onChanged(
+            StandingsCombinedViewState(
+                standingsSeeAll = true,
+                combinedViewState = false
+            )
+        )
+
+        verify(observer).onChanged(
+            StandingsCombinedViewState(
+                standingsSeeAll = false,
+                combinedViewState = true
+            )
+        )
+        verify(observer, times(0)).onChanged(
+            StandingsCombinedViewState(
+                standingsSeeAll = false,
+                combinedViewState = false
+            )
+        )
+        verify(observer, times(0)).onChanged(
+            StandingsCombinedViewState(
+                standingsSeeAll = true,
+                combinedViewState = true
+            )
+        )
+    }
+
+    @Test
+    fun `When onLoadingComplete is called then emit combinedViewState true`() {
+        standingsCombinedViewModel.standingsCombinedViewState.observeForever(observer)
+
+        standingsCombinedViewModel.onLoadingComplete()
+
+        verify(observer).onChanged(StandingsCombinedViewState(combinedViewState = true))
+    }
+}
