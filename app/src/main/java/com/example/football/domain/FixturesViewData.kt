06031e9862b1676ffbfa9a343001b1af116976b4
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-09-28 21:44:27 +0200

    FOOT-49: create on click method for fixture

--- app/src/main/java/com/example/football/domain/FixturesViewData.kt
+++ app/src/main/java/com/example/football/domain/FixturesViewData.kt
@@ -1,6 +1,7 @@
 package com.example.football.domain
 
 data class FixturesViewData(
+    val fixtureId: String,
     val homeTeam: TeamFixtureViewData,
     val awayTeam: TeamFixtureViewData,
     val date: String,
