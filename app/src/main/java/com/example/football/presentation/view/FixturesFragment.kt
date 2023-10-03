a7617b7fe106a1b33c7da8775e470561f8b73140
Joe Malebe <Joe.Malebe@standardbank.co.za>
2023-10-03 19:56:55 +0200

    FOOT-49: create fixture and statistics screen

---
+++ app/src/main/java/com/example/football/presentation/view/FixturesFragment.kt
@@ -0,0 +1,30 @@
+package com.example.football.presentation.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.example.football.presentation.theme.FootballTheme
+import com.example.football.presentation.view.composable.FixtureScreen
+import kotlinx.collections.immutable.persistentListOf
+
+class FixturesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                FootballTheme {
+                    FixtureScreen(persistentListOf(), persistentListOf()) {
+
+                    }
+                }
+            }
+        }
+    }
+}
