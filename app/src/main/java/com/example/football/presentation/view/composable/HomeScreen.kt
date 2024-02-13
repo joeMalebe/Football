320f66cead5d36e88e7cd4f4e3b37e6b30ff9f6e
Joe Malebe <Joe.Malebe@standardbank.co.za>
2024-02-13 21:50:39 +0200

    FOOT-00: remove blue border

--- app/src/main/java/com/example/football/presentation/view/composable/HomeScreen.kt
+++ app/src/main/java/com/example/football/presentation/view/composable/HomeScreen.kt
@@ -4,7 +4,6 @@ import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import android.util.Log
-import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -24,7 +23,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
@@ -174,7 +172,6 @@ fun LeagueList(
                 league = leagues[index],
                 modifier = Modifier
                     .padding(all = 16.dp)
-                    .border(width = 3.dp, color = Color.Blue)
                     .clickable { onLeagueItemClicked(leagues[index]) }
             )
         }
